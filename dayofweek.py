1f8a932f365c06e7ccd5b56bc397e151a8e80850
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-01-15 01:12:36 -0300

    dayofweek.py

---
+++ dayofweek.py
@@ -0,0 +1,16 @@
+def day_of_week(n):
+    if n==1:
+        return 'Monday'
+    elif n==2:
+        return 'Tuesday'
+    elif n==3:
+        return 'Wednesday'
+    elif n==4:
+        return 'Tuesday'
+    elif n==5:
+        return 'Friday'
+    elif n==6:
+        return 'Saturday'
+    elif n==7:
+        return 'Sunday'
+    
