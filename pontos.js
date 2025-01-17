4aab6feddcc53876f93366af56c7ca7ac49b8075
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-01-16 22:18:42 -0300

    Create pontos.js

---
+++ pontos.js
@@ -0,0 +1,6 @@
+let pontos=Math.floor(Math.random() * 200) + 1;
+if (pontos>=100) {
+  alert('Deu certo! A pontuação é igual a: '+pontos);
+} else {
+  alert('Deu errado! A pontuação é igual a: '+pontos);
+}
