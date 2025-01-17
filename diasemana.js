7b9469a6824435d00b7373e8bd7cdd2fd8a17121
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-01-16 21:56:38 -0300

    Create diasemana.js

---
+++ diasemana.js
@@ -0,0 +1,5 @@
+let dia=prompt('Qual o dia da semana?');
+if (dia=='Sábado' || dia == 'Domingo' || dia=='Sabado' || dia == 'sabado' || dia=='sábado') {
+  alert('Bom fim de semana!');
+} else {
+alert('Boa semana!'); }
