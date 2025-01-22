47d2a128de1b646c32d9913ec86062213cd01438
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-01-22 17:32:34 -0300

    Create aura.js

---
+++ aura.js
@@ -0,0 +1,28 @@
+//Criar uma função que exibe "Olá, mundo!" no console.
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+
+function olaMundo() {
+  console.log('Olá, mundo');
+}
+function olaMundo(nome) {
+  console.log(`Olá, ${nome}`);
+}
+function olaMundo(numero) {
+  return 2*numero;
+}
+function olaMundo(numero1,numero3,numero2) {
+  return (numero1+numero3+numero2)/3;
+}
+function olaMundo(numero4,numero5) {
+  if (numero4>numero5) 
+  {
+    return numero5;
+  } return numero4;
+}
+function olaMundo(numero6) {
+  return numero6*numero6;
+}
